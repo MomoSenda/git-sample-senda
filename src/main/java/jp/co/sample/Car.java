74921768a5d884985d8190f131182157eb4e5bad
momosenda <momo.senda@rakus.co.jp>
2018-10-30 11:37:22 +0900

    studentクラスの作成　及び　carクラスに車体の色を追加

--- src/main/java/jp/co/sample/Car.java
+++ src/main/java/jp/co/sample/Car.java
@@ -1,8 +1,18 @@
 package jp.co.sample;
 
 public class Car {
-
+	/**スピード*/
 	private Integer speed;
+	/**車の色*/
+	private String bodyColor;
+
+	public String getBodyColor() {
+		return bodyColor;
+	}
+
+	public void setBodyColor(String bodyColor) {
+		this.bodyColor = bodyColor;
+	}
 
 	public Integer getSpeed() {
 		return speed;
---
+++ src/main/java/jp/co/sample/Student.java
@@ -0,0 +1,23 @@
+package jp.co.sample;
+
+public class Student {
+/** 生徒の名前*/
+	private String name;
+/** 出席番号*/
+	private Integer id;
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public Integer getId() {
+	return id;
+}
+public void setId(Integer id) {
+	this.id = id;
+}
+	
+	
+
+}
