45b874325956291003927083071c48f6b8b6889d
momosenda <momo.senda@rakus.co.jp>
2018-10-30 12:23:54 +0900

    Userクラスを作成

---
+++ src/main/java/jp/co/sample/User.java
@@ -0,0 +1,25 @@
+package jp.co.sample;
+
+public class User {
+/**名前*/
+	private String userName;
+
+/**年齢*/
+	private Integer age;
+
+public String getUserName() {
+	return userName;
+}
+
+public void setUserName(String userName) {
+	this.userName = userName;
+}
+
+public Integer getAge() {
+	return age;
+}
+
+public void setAge(Integer age) {
+	this.age = age;
+}
+}
