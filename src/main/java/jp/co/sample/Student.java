b4b65d60c2232ebb49b8640d09274a313c9b2d28
momosenda <momo.senda@rakus.co.jp>
2018-10-30 11:39:39 +0900

    stuentクラスの修正

--- src/main/java/jp/co/sample/Student.java
+++ src/main/java/jp/co/sample/Student.java
@@ -3,21 +3,14 @@ package jp.co.sample;
 public class Student {
 /** 生徒の名前*/
 	private String name;
-/** 出席番号*/
-	private Integer id;
+
 public String getName() {
 	return name;
 }
 public void setName(String name) {
 	this.name = name;
 }
-public Integer getId() {
-	return id;
-}
-public void setId(Integer id) {
-	this.id = id;
-}
-	
+
 	
 
 }
